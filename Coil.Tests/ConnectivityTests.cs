687145b87a9477d45eeec4adaeb739c56420a033
Fae Burns <fae.burns0@gmail.com>
2023-03-06 12:33:06 +0000

    Connectivity and WireValue preemptive tests

---
+++ Coil.Tests/ConnectivityTests.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Coil.Tests
+{
+    [TestFixture]
+    public class ConnectivityTests
+    {
+        private void AssertContainsAll(IReadOnlyCollection<Wire> collection, params Wire[] wires)
+        {
+            Assert.AreEqual(wires.Length, collection.Count);
+            
+            foreach (Wire wire in wires)
+            {
+                Assert.Contains(wire, (ICollection<Wire>)collection);
+            }
+        }
+        
+        [Test]
+        public void Connect_2()
+        {
+            ConnectionManager connectionManager = new ConnectionManager();
+            
+            Wire wire = new Wire();
+            Wire wire2 = new Wire();
+
+            connectionManager.Connect(wire, wire2);
+
+            AssertContainsAll(connectionManager.GetConnections(wire), wire2);
+            AssertContainsAll(connectionManager.GetConnections(wire2), wire);
+        }
+
+        [Test]
+        public void Connect_3()
+        {
+            ConnectionManager connectionManager = new ConnectionManager();
+            
+            Wire wire = new Wire();
+            Wire wire2 = new Wire();
+            Wire wire3 = new Wire();
+
+            connectionManager.Connect(wire, wire2);
+            connectionManager.Connect(wire2, wire3);
+
+            AssertContainsAll(connectionManager.GetConnections(wire), wire2);
+            AssertContainsAll(connectionManager.GetConnections(wire), wire3);
+            
+            AssertContainsAll(connectionManager.GetConnections(wire2), wire);
+            AssertContainsAll(connectionManager.GetConnections(wire2), wire3);
+            
+            AssertContainsAll(connectionManager.GetConnections(wire3), wire);
+            AssertContainsAll(connectionManager.GetConnections(wire3), wire2);
+        }
+        
+        [Test]
+        public void Connect_3_NonLinear()
+        {
+            ConnectionManager connectionManager = new ConnectionManager();
+            
+            Wire wire = new Wire();
+            Wire wire2 = new Wire();
+            Wire wire3 = new Wire();
+
+            connectionManager.Connect(wire, wire2);
+            connectionManager.Connect(wire, wire3);
+
+            AssertContainsAll(connectionManager.GetConnections(wire), wire2);
+            AssertContainsAll(connectionManager.GetConnections(wire), wire3);
+            
+            AssertContainsAll(connectionManager.GetConnections(wire2), wire);
+            AssertContainsAll(connectionManager.GetConnections(wire2), wire3);
+            
+            AssertContainsAll(connectionManager.GetConnections(wire3), wire);
+            AssertContainsAll(connectionManager.GetConnections(wire3), wire2);
+        }
+        
+        [Test]
+        public void Connect_Self()
+        {
+            ConnectionManager connectionManager = new ConnectionManager();
+            
+            Wire wire = new Wire();
+
+            ArgumentException exception = Assert.Throws<ArgumentException>(connectionManager.Connect(wire, wire));
+            Assert.That(exception.Message, Is.EqualTo("Cannot connect a wire to itself"));
+        }
+
+        [Test]
+        public void Disconnect_2()
+        {
+            ConnectionManager connectionManager = new ConnectionManager();
+            
+            Wire wire = new Wire();
+            Wire wire2 = new Wire();
+
+            connectionManager.Connect(wire, wire2);
+            connectionManager.Disconnect(wire, wire2);
+
+            // check that there are no connections
+            AssertContainsAll(connectionManager.GetConnections(wire));
+            AssertContainsAll(connectionManager.GetConnections(wire2));
+        }
+        
+        [Test]
+        public void Disconnect_2_Reverse()
+        {
+            ConnectionManager connectionManager = new ConnectionManager();
+            
+            Wire wire = new Wire();
+            Wire wire2 = new Wire();
+
+            connectionManager.Connect(wire, wire2);
+            
+            // disconnect in reverse order 
+            connectionManager.Disconnect(wire2, wire);
+
+            // check that there are no connections
+            AssertContainsAll(connectionManager.GetConnections(wire));
+            AssertContainsAll(connectionManager.GetConnections(wire2));
+        }
+
+        [Test]
+        public void Disconnect_3()
+        {
+            ConnectionManager connectionManager = new ConnectionManager();
+
+            Wire wire = new Wire();
+            Wire wire2 = new Wire();
+            Wire wire3 = new Wire();
+
+            connectionManager.Connect(wire, wire2);
+            connectionManager.Connect(wire2, wire3);
+
+            // disconnect wires 1 and 2
+            connectionManager.Disconnect(wire, wire2);
+            
+            AssertContainsAll(connectionManager.GetConnections(wire));
+            AssertContainsAll(connectionManager.GetConnections(wire2), wire3);
+            AssertContainsAll(connectionManager.GetConnections(wire3), wire2);
+        }
+
+        [Test]
+        public void Disconnect_Circular_NonBreak()
+        {
+            ConnectionManager connectionManager = new ConnectionManager();
+
+            Wire wire = new Wire();
+            Wire wire2 = new Wire();
+            Wire wire3 = new Wire();
+
+            connectionManager.Connect(wire, wire2);
+            connectionManager.Connect(wire2, wire3);
+            connectionManager.Connect(wire3, wire);
+
+            // disconnect wires 1 and 2
+            connectionManager.Disconnect(wire, wire2);
+            
+            AssertContainsAll(connectionManager.GetConnections(wire), wire2, wire3);
+            AssertContainsAll(connectionManager.GetConnections(wire2), wire, wire3);
+            AssertContainsAll(connectionManager.GetConnections(wire3), wire, wire2);
+        }
+        
+        [Test]
+        public void Disconnect_Circular_Break()
+        {
+            ConnectionManager connectionManager = new ConnectionManager();
+
+            Wire wire = new Wire();
+            Wire wire2 = new Wire();
+            Wire wire3 = new Wire();
+
+            connectionManager.Connect(wire, wire2);
+            connectionManager.Connect(wire2, wire3);
+            connectionManager.Connect(wire3, wire);
+
+            // disconnect wires 1 and 2
+            connectionManager.Disconnect(wire, wire2);
+            
+            // everything should still be connected
+            AssertContainsAll(connectionManager.GetConnections(wire), wire2, wire3);
+            AssertContainsAll(connectionManager.GetConnections(wire2), wire, wire3);
+            AssertContainsAll(connectionManager.GetConnections(wire3), wire, wire2);
+        }
+    }
+}
---
+++ Coil.Tests/WireValueTests.cs
@@ -0,0 +1,78 @@
+﻿using System.Runtime.Remoting.Messaging;
+using NUnit.Framework;
+
+namespace Coil.Tests
+{
+    [TestFixture]
+    public class WireValueTests
+    {
+        [Test]
+        public void InitialValue()
+        {
+            Wire wire = new Wire();
+            Assert.IsFalse(wire.Peek().Value);
+        }
+        
+        [Test]
+        public void StoresValue()
+        {
+            Wire wire = new Wire();
+
+            wire.Push(new BoolValue(false));
+            Assert.IsFalse(wire.Peek().Value);
+            
+            wire.Push(new BoolValue(true));
+            Assert.IsTrue(wire.Peek().Value);
+            
+            wire.Push(new BoolValue(false));
+            Assert.IsFalse(wire.Peek().Value);
+        }
+
+        [Test]
+        public void ConnectsValue_2()
+        {
+            ConnectionManager connectionManager = new ConnectionManager();
+            
+            Wire wire = new Wire();
+            Wire wire2 = new Wire();
+
+            connectionManager.Connect(wire, wire2);
+
+            // check that value on wire2 changes when pushed to wire1
+            Assert.IsFalse(wire2.Peek().Value);
+            wire.Push(new BoolValue(true));
+            Assert.IsTrue(wire2.Peek().Value);
+        }
+        
+        [Test]
+        public void ConnectsValue_3()
+        {
+            ConnectionManager connectionManager = new ConnectionManager();
+
+            Wire wire = new Wire();
+            Wire wire2 = new Wire();
+            Wire wire3 = new Wire();
+
+            connectionManager.Connect(wire, wire2);
+            connectionManager.Connect(wire2, wire3);
+            
+            // check that value on wire3 changes when pushed to wire1
+            Assert.IsFalse(wire3.Peek().Value);
+            wire.Push(new BoolValue(true));
+            Assert.IsTrue(wire3.Peek().Value);
+        }
+
+        [Test]
+        public void PushPriority()
+        {
+            Wire wire = new Wire();
+
+            wire.Push(new BoolValue(true));
+            Assert.IsTrue(wire.Peek().Value);
+
+            // check that value does not change to false when the value is already true
+            wire.Push(new BoolValue(false));
+            Assert.IsTrue(wire.Peek().Value);
+        }
+    }
+}
